36ef3b58a1a23618089eec07b3d4e7d910386b29
Sylvia <sylviakieha@gmail.com>
2015-08-20 16:08:00 +0300

    added gemset and dep

--- app.rb
+++ app.rb
@@ -2,6 +2,12 @@ require "cuba"
 
 Cuba.define do
   on root do
-    res.write("Hello Frogger!")
+    res.write("
+    <html>
+     <body>
+      <h1>Hey There!</h1>
+     </body>
+    </html>
+    ")
   end
 end
